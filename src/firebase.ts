45dfffc39402a821d47a622ef52f82fae525f1bf
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-20 20:10:14 +0200

    Adding firebase and firebase storage bucket config

---
+++ src/firebase.ts
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: 'AIzaSyAPCWDwJEHEDGz_s2Si39cIO-KGa_iJCa4',
+  authDomain: 'react-slack-clone-4b014.firebaseapp.com',
+  databaseURL: 'https://react-slack-clone-4b014.firebaseio.com',
+  projectId: 'react-slack-clone-4b014',
+  storageBucket: 'react-slack-clone-4b014.appspot.com',
+  appId: '1:677245625699:web:3d13f1e5aaae7b43b2df56',
+  measurementId: 'G-E8Q0WP36CQ',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export default firebase;
