f02ee12ce9ae5b30924eb671eff9ebc656538b37
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-20 20:51:49 +0200

    Creating a Form For Registering User

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './Components/App';
 import Login from './Components/Auth/Login';
 import Register from './Components/Auth/Register';
 import * as serviceWorker from './serviceWorker';
+
+import 'semantic-ui-css/semantic.min.css';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
