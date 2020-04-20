c9be4e92c3a6d376315454b07472e225cabbcd64
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-20 19:55:00 +0200

    Adding routing and Bootstrapping Components

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App';
+import Login from './Components/Auth/Login';
+import Register from './Components/Auth/Register';
 import * as serviceWorker from './serviceWorker';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route exact path='/' component={App} />
+      <Route path='/login' component={Login} />
+      <Route path='/register' component={Register} />
+    </Switch>
+  </Router>
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
