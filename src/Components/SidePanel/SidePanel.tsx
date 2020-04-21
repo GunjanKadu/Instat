1b798c13e5c5abfbb2601e64400e40aacb771bda
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-22 00:18:48 +0200

    Adding an side panel

--- src/Components/SidePanel/SidePanel.tsx
+++ src/Components/SidePanel/SidePanel.tsx
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
-
+import { Menu } from 'semantic-ui-react';
+import UserPanel from './UserPanel/UserPanel';
 class SidePanel extends Component {
   render() {
-    return <div>SidePanel</div>;
+    return (
+      <Menu
+        size='large'
+        inverted
+        fixed='left'
+        vertical
+        style={{ background: '#3a133e', fontSize: '1.2rem' }}
+      >
+        <UserPanel />
+      </Menu>
+    );
   }
 }
 
