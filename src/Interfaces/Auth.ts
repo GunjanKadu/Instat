f4ea3cc653c38c058561b4279d124e85f08d2260
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-21 20:39:35 +0200

    Adding interface for firebase response

--- src/Interfaces/Auth.ts
+++ src/Interfaces/Auth.ts
@@ -16,3 +16,44 @@ export interface ILogin {
   errors: Array<{ message: string }>;
   loading: boolean;
 }
+export interface IUser {
+  N?: Array<any>;
+  l?: string;
+  m?: string;
+  s?: string;
+  a?: any;
+  b?: any;
+  xa?: string;
+  _lat?: string;
+  refreshToken?: string;
+  uid?: string;
+  displayName?: string;
+  photoURL?: string;
+  email?: string;
+  emailVerified?: boolean;
+  phoneNumber?: string;
+  isAnonymous?: boolean;
+  tenantId?: any;
+  metadata?: any;
+  providerData?: any;
+  wa?: boolean;
+  na?: any;
+  u?: any;
+  Yb?: any;
+  eb?: any;
+  P?: boolean;
+  i?: any;
+  R?: any;
+  h?: any;
+  B?: any;
+  Z?: any;
+  oa?: any;
+  za?: any;
+  aa?: any;
+  W?: any;
+  ya?: any;
+  $?: any;
+  O?: any;
+  multiFactor?: any;
+  ga?: any;
+}
