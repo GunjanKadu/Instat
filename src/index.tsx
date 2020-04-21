2680c478fbabb4c20479c2be493ddd398cfa2e71
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-21 19:00:44 +0200

    Redirecting Logged in users to main page

--- src/index.tsx
+++ src/index.tsx
@@ -6,20 +6,44 @@ import Register from './Components/Auth/Register';
 import * as serviceWorker from './serviceWorker';
 
 import 'semantic-ui-css/semantic.min.css';
+import firebase from './firebase';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter,
+  RouteComponentProps,
+} from 'react-router-dom';
 
-const Root = () => (
+export class Root extends React.Component<RouteComponentProps> {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.props.history.push('/');
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Switch>
+        <Route exact path='/' component={App} />
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
+      </Switch>
+    );
+  }
+}
+
+const RootWithAuth = withRouter(Root);
+
+ReactDOM.render(
   <Router>
-    <Switch>
-      <Route exact path='/' component={App} />
-      <Route path='/login' component={Login} />
-      <Route path='/register' component={Register} />
-    </Switch>
-  </Router>
+    <RootWithAuth />{' '}
+  </Router>,
+  document.getElementById('root')
 );
-
-ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
