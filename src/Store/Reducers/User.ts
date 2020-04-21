c18855f156eeb5b25227318ea09e91ee8251b72a
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-21 20:40:11 +0200

    user Reducer

---
+++ src/Store/Reducers/User.ts
@@ -0,0 +1,23 @@
+import * as actionTypes from '../Actions/types';
+import * as I from '../../Interfaces/UserReducer';
+import { IAction } from '../../Interfaces/Action';
+
+const initialState: I.IUserState = {
+  current: null,
+  isLoading: true,
+};
+
+export const userReducer = (
+  state: I.IUserState = initialState,
+  action: IAction
+): I.IUserState => {
+  switch (action.type) {
+    case actionTypes.SET_USER:
+      return {
+        current: action.payload.currentUser,
+        isLoading: false,
+      };
+    default:
+      return state;
+  }
+};
