b03c1fe4e94d08101b085f824d35f256f809d20d
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-23 23:36:43 +0200

    Rendering the image in the chat

--- src/Components/Messages/Message/Message.tsx
+++ src/Components/Messages/Message/Message.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Comment } from 'semantic-ui-react';
+import { Comment, Image } from 'semantic-ui-react';
 import * as I from '../../../Interfaces/Messages';
 import { IUser } from '../../../Interfaces/Auth';
 import moment from 'moment';
@@ -9,13 +9,24 @@ export default function Message({ message, user }: I.IPropsForMessage) {
     return message.user.id === user.uid ? 'message_self' : '';
   };
   const timeFromNow = (timestamp: {}) => moment(timestamp).fromNow();
+
+  const isImage = (Message: I.IMessage): boolean => {
+    return (
+      message.hasOwnProperty('image') && !message.hasOwnProperty('content')
+    );
+  };
+
   return (
     <Comment>
       <Comment.Avatar src={message.user.avatar} />
       <Comment.Content className={isOwnMessage(message, user)}>
         <Comment.Author as='a'>{message.user.name}</Comment.Author>
         <Comment.Metadata>{timeFromNow(message.timestamp)}</Comment.Metadata>
-        <Comment.Text>{message.content}</Comment.Text>
+        {isImage(message) ? (
+          <Image src={message.image} className='message_image' />
+        ) : (
+          <Comment.Text>{message.content}</Comment.Text>
+        )}
       </Comment.Content>
     </Comment>
   );
