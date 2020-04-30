0267075615a35172263e319796a507cd7b59bf5a
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-30 18:42:54 +0200

    Creating change Avatar Modal

--- src/Components/SidePanel/UserPanel/UserPanel.tsx
+++ src/Components/SidePanel/UserPanel/UserPanel.tsx
@@ -9,7 +9,17 @@
  */
 
 import React, { Component } from 'react';
-import { Grid, Header, Icon, Dropdown, Image } from 'semantic-ui-react';
+import {
+  Grid,
+  Header,
+  Icon,
+  Dropdown,
+  Image,
+  Modal,
+  Input,
+  GridColumn,
+  Button,
+} from 'semantic-ui-react';
 import firebase from '../../../firebase';
 import { TUser } from '../../../Interfaces/Auth';
 
@@ -19,12 +29,17 @@ interface IStateProps {
 }
 interface IState {
   user: TUser;
+  modal?: boolean;
 }
 
 class UserPanel extends Component<IStateProps, IState> {
   state = {
     user: this.props.currentUser,
+    modal: false,
   };
+  openModal = () => this.setState({ modal: true });
+
+  closeModal = () => this.setState({ modal: false });
 
   dropDownOption = () => [
     {
@@ -36,7 +51,10 @@ class UserPanel extends Component<IStateProps, IState> {
       key: 'user',
       disabled: true,
     },
-    { text: <span>Change Avatar</span>, key: 'avatar' },
+    {
+      text: <span onClick={this.openModal}>Change Avatar</span>,
+      key: 'avatar',
+    },
     {
       text: <span onClick={this.handleSignout}>Sign Out</span>,
       key: 'signout',
@@ -50,7 +68,7 @@ class UserPanel extends Component<IStateProps, IState> {
       .then(() => console.log('signed out!'));
   };
   render() {
-    const { user } = this.state;
+    const { user, modal } = this.state;
     const { primaryColor } = this.props;
     return (
       <Grid style={{ background: primaryColor }}>
@@ -74,6 +92,35 @@ class UserPanel extends Component<IStateProps, IState> {
               />
             </Header>
           </Grid.Row>
+          {/* Change user avatar modal */}
+          <Modal basic open={modal} onClose={this.closeModal}>
+            <Modal.Header>Change Avatar</Modal.Header>
+            <Modal.Content>
+              <Input fluid type='file' label='New Avatar' name='previewImage' />
+              <Grid centered stackable columns={2}>
+                <Grid.Row centered>
+                  <GridColumn className='ui center aligned grid'>
+                    {/* Image Preview */}
+                  </GridColumn>
+                  <GridColumn>{/* Cropped Image Preview */}</GridColumn>
+                </Grid.Row>
+              </Grid>
+            </Modal.Content>
+            <Modal.Actions>
+              <Button color='green' inverted>
+                <Icon name='save' />
+                Change Avatar
+              </Button>
+              <Button color='green' inverted>
+                <Icon name='image' />
+                Preview
+              </Button>
+              <Button onClick={this.closeModal} color='red' inverted>
+                <Icon name='remove' />
+                Cancel
+              </Button>
+            </Modal.Actions>
+          </Modal>
         </Grid.Column>
       </Grid>
     );
